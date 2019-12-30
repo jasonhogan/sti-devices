0cd914e834294869ffb7b0415bd6c0cc56943496
sr-server <hoganphysics@gmail.com>
2019-12-29 16:49:56 -0800

    Initialize FPGA works
    * Using the Counters.bit temporarily (works)
    * Error after loading firmware because Frontpanel interface has been damaged (ESD likely)
    
    Jason

--- devices/XEM7310/writebus/src/main.cpp
+++ devices/XEM7310/writebus/src/main.cpp
@@ -1,8 +1,20 @@
 
 #include <iostream>
 
-int main()
+#include "okFrontPanelDLL.h"
+
+#include "frontpanelsupport.h"
+
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+
+	// Initialize the FPGA with our configuration bitfile.
+	OpalKelly::FrontPanelPtr dev = initializeFPGA("Counters.bit");
+	if (!dev.get()) {
+		printf("FPGA could not be initialized.\n");
+		return -1;
+	}
+
+	return 0;
 }
 
