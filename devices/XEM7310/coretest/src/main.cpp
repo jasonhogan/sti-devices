88af2465830d31f0b6d3cf1eaeaf545b3aff14b9
sr-server <hoganphysics@gmail.com>
2020-01-29 20:15:09 -0800

    Test of timing_module firmware
    * Added a c++ project to load the timing_module with events using the OK api and then assert play
    * The firmware works mostly as expected. The digital pattern generation is correct and the delay times seem reasonable.
    * The digital board responds well, but it may be the bottleneck for 5 ns changes.  It looks like there is a few extra ns of delay before a falling edge, affecting duty cycle.
    * There is pin to pin jitter at the 1 ns level coming out of the fpga (seen shot to shot).  Timing constraints need to be looked into (so far nothing has been done).
    
    Jason

---
+++ devices/XEM7310/coretest/src/main.cpp
@@ -0,0 +1,124 @@
+
+#include <iostream>
+#include <bitset>
+
+#include "okFrontPanelDLL.h"
+#include "frontpanelsupport.h"
+
+UINT32 makeBitValue(const std::string& bits, bool enable = true)
+{
+	std::bitset<26> output(bits);
+	output.set(24, enable);	//enable
+
+	return output.to_ulong();
+}
+
+class RegEvent
+{
+public:
+	RegEvent(UINT32 addr, UINT32 time, const std::string& bits, char opcode) : addr(addr), time(time), opcode(opcode)
+	{
+		operand = makeBitValue(bits);
+	}
+
+	RegEvent(UINT32 time, const std::string& bits, char opcode) : RegEvent(0, time, bits, opcode) {}
+
+	UINT32 getValue() const
+	{
+		return (opcode << 28) + operand;
+	}
+
+	UINT32 time;
+	UINT32 operand;
+	UINT32 addr;
+	char opcode;
+};
+
+void addRegEvent(std::vector<okTRegisterEntry>& regs, const RegEvent& regEvent)
+{
+	okTRegisterEntry reg;
+
+
+	reg.address = regEvent.addr;
+	reg.data = regEvent.getValue();
+	regs.push_back(reg);	//add value
+
+	reg.address = regEvent.addr + 1;
+	reg.data = regEvent.time;
+	regs.push_back(reg);	//add time
+
+}
+
+
+
+void printRegData(const std::vector<okTRegisterEntry>& regs)
+{
+	for (unsigned j = 0; j < regs.size(); ++j) {
+		std::cout << std::hex 
+			<< "< x"
+			<< regs.at(j).address << ", x"
+			<< regs.at(j).data << " >" << std::endl;
+	}
+	std::cout << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+	// Initialize the FPGA with our configuration bitfile.
+	OpalKelly::FrontPanelPtr dev = initializeFPGA("timing_mod_test_top.bit");
+	if (!dev.get()) {
+		printf("FPGA could not be initialized.\n");
+		return -1;
+	}
+
+	std::vector<RegEvent> rawEvents;
+	std::vector<okTRegisterEntry> writeRegs;
+	
+	//                     time, value, opcode
+	rawEvents.emplace_back(10, "00000000", 0); 
+	rawEvents.emplace_back(0, "00000001", 0);
+	rawEvents.emplace_back(0, "00000010", 0);
+	rawEvents.emplace_back(0, "00000100", 0);
+	rawEvents.emplace_back(0, "00000010", 0);
+	rawEvents.emplace_back(0, "00000000", 0);
+	rawEvents.emplace_back(0, "00000010", 0);
+	rawEvents.emplace_back(0, "00000000", 0);
+	rawEvents.emplace_back(0, "00000000", 2);	//Stop
+
+	unsigned initaddr = 0;
+
+	//define addresses and convert to okRegisters
+	for (unsigned i = initaddr; i < rawEvents.size(); ++i) {
+		rawEvents.at(i).addr = 2 * i;
+
+		addRegEvent(writeRegs, rawEvents.at(i));	//adds value and time on sequential addresses
+	}
+
+	printRegData(writeRegs);
+
+
+	int tmp;
+//	std::cout << "write reg? ";
+//	std::cin >> tmp;
+
+	dev->WriteRegisters(writeRegs);
+	
+//	std::cout << "reset? ";
+//	std::cin >> tmp;
+
+//	dev->ActivateTriggerIn(0x40, 1);	//reset
+	
+	std::cout << "play? ";
+	std::cin >> tmp;
+
+	while (true) {
+
+
+
+		dev->ActivateTriggerIn(0x40, 0);	//trigger the sequence
+
+	}
+
+	return 0;
+}
+
